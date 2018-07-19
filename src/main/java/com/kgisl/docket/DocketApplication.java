18eea27d4b98d82b4e24e522e676e2368dbab5fc
baraneetharan <baraneetharan@live.com>
2018-07-19 18:12:26 +0530

    initial commit

---
+++ src/main/java/com/kgisl/docket/DocketApplication.java
@@ -0,0 +1,12 @@
+package com.kgisl.docket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocketApplication.class, args);
+	}
+}
